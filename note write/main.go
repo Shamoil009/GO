144e806d54abd0a036bedaf94130267d0ff6c2a1
Shamoil009 <shamoilkhan888@gmail.com>
2024-08-11 21:42:16 +0500

    made note app using struct

--- note write/main.go
+++ note write/main.go
@@ -1,18 +1,60 @@
 package main
 
-import "fmt"
+import (
+	// "errors"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"example.com/note/note"
+)
 
 func main() {
+	noteTitle, noteContent := getNoteData()
 
-	noteTitle:=getNoteData("Note Title: ")
-	noteContent:=getNoteData("Note Content: ")
+	var userNote *note.Note
+	userNote, err := note.New(noteTitle, noteContent)
 
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	userNote.Display()
+	err = userNote.Save()
+	if err != nil {
+		fmt.Println("Saving note failed")
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Saving note succeeded!")
 
 }
 
-func getNoteData(promtText string) string {
-	fmt.Println(promtText)
-	var value string
-	fmt.Scanln(&value)
-	return value
+func getNoteData() (string, string) {
+	noteTitle := getUserInput("Note Title: ")
+	noteContent := getUserInput("Note Content: ")
+
+	return noteTitle, noteContent
+}
+
+func getUserInput(promtText string) string {
+	fmt.Print(promtText)
+	// var value string
+
+	// Scanln() can't handle long text input
+	// fmt.Scanln(&value)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
 }
--- note write/note/note.go
+++ note write/note/note.go
@@ -1,24 +1,45 @@
 package note
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 )
 
 type Note struct {
-	title      string
-	content    string
-	created_at time.Time
+	Title      string `json:"title"`
+	Content    string `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 func (note Note) Display() {
-	fmt.Printf("Your note titled %v has the following content:\n\n%v\n\n", note.title, note.content)
+	fmt.Printf("Your note titled %v has the following content:\n\n%v\n\n", note.Title, note.Content)
 }
 
-func NewNote(title, content string) (*Note, error) {
+func New(title, content string) (*Note, error) {
+
+	if title == "" || content == "" {
+		return &Note{}, errors.New("invalid input")
+	}
+
 	return &Note{
-		title:      title,
-		content:    content,
-		created_at: time.Now(),
+		Title:      title,
+		Content:    content,
+		CreatedAt: time.Now(),
 	}, nil
+}
+
+func (note Note) Save() error {
+	fileName := strings.ReplaceAll(note.Title, " ", "_")
+	fileName = strings.ToLower(fileName) + ".json"
+
+	json, err := json.Marshal(note) // marshal require public fields
+
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(fileName, json, 0644) // writeFile return err also
 }
