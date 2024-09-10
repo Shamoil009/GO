f72454b10e12d9350180918ee283d5e60b4560b2
Shamoil Khan <shamoilkhan888@gmail.com>
2024-09-11 00:59:39 +0500

    learning array

--- array slices map/main.go
+++ array slices map/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	var productNames [4]string
+	productNames = [4]string{"A book"}
 	prices := [4]float64{23.99, 32.4, 21.4, 54}
 	fmt.Println(prices)
+	productNames[2]="A carpet"
+	fmt.Println(productNames)
 }
