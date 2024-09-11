b82474e4d08678e05887fd37bb7b2ce74c0c5a97
Shamoil Khan <shamoilkhan888@gmail.com>
2024-09-11 20:04:04 +0500

    learning array

--- array slices map/main.go
+++ array slices map/main.go
@@ -7,6 +7,17 @@ func main() {
 	productNames = [4]string{"A book"}
 	prices := [4]float64{23.99, 32.4, 21.4, 54}
 	fmt.Println(prices)
-	productNames[2]="A carpet"
+	productNames[2] = "A carpet"
 	fmt.Println(productNames)
+
+	// 1 index to 2
+	featuredPrices := prices[1:3]
+
+	// start index to 2
+	// featuredPrices:=prices[:3]
+
+	// start 1 index to end
+	// featuredPrices:=prices[1:]
+
+	fmt.Println(featuredPrices)
 }
